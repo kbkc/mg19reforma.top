c7594fa3555d212c4358d06cae30a44874c3cb66
SebastianTomala <sebastian.tomala@webdesignsolutions.pl>
2019-11-06 13:51:08 +0100

    usuniecie store name z nazwy sklepu

--- app/code/local/Smartbees/Inpost/Model/Sales/Order.php
+++ app/code/local/Smartbees/Inpost/Model/Sales/Order.php
@@ -4,8 +4,8 @@ class Smartbees_Inpost_Model_Sales_Order extends Mage_Sales_Model_Order{
 		$desc = parent::getShippingDescription();
 		$pickupObject = $this->getPickupObject();
 		if($pickupObject){
-			$desc .= ' Store: '.$pickupObject->getStore();
-			$desc .= ' Name: '.$pickupObject->getName();
+			$desc .= $pickupObject->getStore();
+			$desc .= $pickupObject->getName();
 			$desc .= '';
 		}
 		return $desc;
